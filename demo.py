76b416c68799e2ec52873a06dfb54a35db40d80c
Jongkook Choi <jongkook90@gmail.com>
2020-11-23 16:34:12 +0900

    Final commit

--- demo.py
+++ demo.py
@@ -8,3 +8,8 @@ input('End of solution 1. Press Enter to continue...')
 problem_2.verbose = True
 problem_2.Problem2().solve()
 input('End of solution 2. Press Enter to continue...')
+
+with open('problem_3.py') as f:
+    code = compile(f.read(), 'problem_3.py', 'exec')
+    exec(code)
+input('End of solution 3. Press Enter to continue...')
